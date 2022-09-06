7fc240333d655e19c555448939b56ae37e645dd8
Me1 <Me1@DESKTOP-02SL8LJ>
2022-09-06 12:04:01 +0300

    Add project files.

---
+++ Lesson01_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson01_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Labas Pasauli! 3 + 5 = {3 + 5}"); // 1 vietoje
+            Console.WriteLine(String.Format("3 + 5  = {0}", 3 + 5)); // 2 vietoje 
+            Console.WriteLine("Labas Pasauli! = {3 + 5}");
+            Console.WriteLine("Labas Pasauli!");
+            Console.WriteLine(3 + 5);
+            Console.WriteLine("\"3 +  5 = " + "5" + (3+5)); // 3 vietoje
+
+            string inputText;
+            Console.WriteLine("Kokia Jūsų mėgstamiausia spalva");
+            inputText = Console.ReadLine();
+            Console.WriteLine($"Mano irgi {inputText}!");
+
+
+        }
+    }
+}
